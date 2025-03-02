193d517d47f1ee7f8f2136b6eaf902c2f7bad362
Yeonwoo Jang <yjang385@gmail.com>
2025-03-02 16:28:07 +0000

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,7 +1,9 @@
 set -e 
 
 curl -LsSf https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
+export PATH=$PATH:$HOME/.local/bin/env
 
+# $HOME/.local/bin/uv venv --python=3.10
 uv venv --python=3.10
 source .venv/bin/activate
+uv pip install -r requirements.txt
