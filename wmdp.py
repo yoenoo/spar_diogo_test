8a69bf4d0a5b62c995edf07ad3e2b1508bea9f79
Yeonwoo Jang <yjang385@gmail.com>
2025-03-02 17:01:07 +0000

    cleanup wmdp

--- wmdp.py
+++ wmdp.py
@@ -7,18 +7,8 @@ from inspect_ai.scorer import choice
 from inspect_ai.solver import multiple_choice, system_message
 from inspect_ai.model import ChatMessageAssistant, ChatMessageSystem, ChatMessageUser
 
-
-
-# from inspect_ai.dataset import Sample, hf_dataset, Dataset
-# from inspect_ai.model import ChatMessageAssistant, ChatMessageSystem, ChatMessageUser
-# from inspect_ai.scorer import match, model_graded_fact, Scorer, answer
-# from inspect_ai.solver import chain_of_thought, generate, self_critique, Generate, Solver, TaskState, chain, solver, Choices
-
-# from datasets import load_dataset
-# ds = load_dataset("cais/wmdp", "wmdp-bio") # or wmdp-chem / wmdp-cyber
-# pprint(ds["test"][0])
-
 """
+wmdp-bio record example:
 {'answer': 0,
  'choices': ['SpyCEP from Streptococcus pyogenes',
              'Eremophilane O-acetyltransferase from Penicillium roqueforti',
@@ -28,12 +18,11 @@ from inspect_ai.model import ChatMessageAssistant, ChatMessageSystem, ChatMessag
              'a bacterium more harmful?'}
 """
 
-SYSTEM_MESSAGE = """"""
-
 def record_to_sample(record: dict[str, Any]) -> Sample:
   input = [ChatMessageUser(content=record["question"])]
   choices = record["choices"]
-  target = chr(ord("A") + record["answer"])
+  # target = chr(ord("A") + record["answer"])
+  target = ("ABCD"[record["answer"]])
   return Sample(input=input, choices=choices, target=target)
 
 def load_wmdp_dataset(name: str) -> Dataset:
@@ -47,7 +36,8 @@ def load_wmdp_dataset(name: str) -> Dataset:
 def create_task(dataset_name: str) -> Task:
   return Task(
     dataset=load_wmdp_dataset(dataset_name),
-    solver=[system_message(SYSTEM_MESSAGE), multiple_choice()],
+    # solver=[system_message(SYSTEM_MESSAGE), multiple_choice()],
+    solver=multiple_choice(),
     scorer=choice(),
   )
 
@@ -63,93 +53,20 @@ def wmdp_chem() -> Task:
 def wmdp_cyber() -> Task:
   return create_task("wmdp-cyber")
 
-ds = load_wmdp_dataset("wmdp-bio")
-pprint(ds.samples[0].__dict__)
 
-# log = eval(wmdp_bio(), model="openai/gpt-4o-mini", limit=5)
+if __name__ == "__main__":
+  base_model = "HuggingFaceH4/zephyr-7b-beta"
+  unlearned_model = "cais/Zephyr_RMU"
 
-# from transformers import AutoTokenizer, AutoModelForCausalLM
-# tokenizer = AutoTokenizer.from_pretrained("cais/Zephyr_RMU")
-# model = AutoModelForCausalLM.from_pretrained("cais/Zephyr_RMU")
+  provider = "vllm" # or "hf"
+  
+  for m in [base_model, unlearned_model]:
+    model = f"{provider}/{m}"
+    print(f"Using model {model}")
 
-# log = eval(wmdp_bio(), model="hf/cais/Zephyr_RMU", model_args=dict(device="cuda:0"))
+    log = eval(wmdp_bio(), model=model, limit=10)
+    # log = eval(wmdp_bio(), model=model, limit=10, model_args={"tensor_parallel_size": 2}) # slow!
+    print(log)
 
-base_model = "HuggingFaceH4/zephyr-7b-beta"
-unlearned_model = "cais/Zephyr_RMU"
-
-provider = "hf" 
-# provider = "vllm"
-
-model = f"{provider}/{unlearned_model}"
-print(f"Using model {model}")
-
-log = eval(wmdp_bio(), model=model)
-print(log)
-
-exit()
-
-# make it progressively more complex / robust
-## chatmessageuser -> this thing?
-## system message
-## chain_of_thought? self_critique? different prompting techniques
-
-
-def test_my_solver(solver: Solver, dataset: Dataset, n: int = 5, scorer: Scorer = match()):
-  @task
-  def test_task() -> Task:
-    return Task(dataset=dataset, solver=solver, scorer=scorer)
-
-  log = eval(test_task(), model="openai/gpt-4o-mini", limit=n)
-  return log 
-
-def record_to_sample(record: dict[str, Any]) -> Sample:
-  input = [ChatMessageUser(content=record["question"])]
-  choices = record["choices"]
-  target = str(record["answer"])
-  return Sample(input=input, choices=choices, target=target)
-
-my_dataset = hf_dataset(
-  path="cais/wmdp", 
-  name="wmdp-bio",
-  sample_fields=record_to_sample,
-  split="test",
-)
-pprint(my_dataset.samples[0].__dict__)
-
-
-# TEMPLATE_MCQ = r"""
-# Answer the following multiple choice question. The entire content of your response should be of the following format: 'ANSWER: $LETTERS' (without quotes) where LETTERS is one or more of {letters}.
-
-# {question}
-
-# {choices}"""
-
-def letters_and_answer_options(choices: Choices) -> tuple[str, str]:
-  letters = list(map(str, range(len(choices))))
-  return (
-    ", ".join(letters),
-    "\n".join([f"({letter}) {choice.value}" for letter, choice in zip(letters, choices)])
-  )
-
-@solver 
-def multiple_choice_format(template: str = TEMPLATE_MCQ) -> Solver:
-  tags = set(re.findall(r"\{.*?\}", template))
-  assert r"{question}" in tags, "Template must include {question} field"
-  assert r"{choices}" in tags, "Template must include {choices} field" 
-  assert tags - {r"{question}", r"{choices}", r"{letters}"} == set(), "Unexpected field found in template"
-
-  async def solve(state: TaskState, generate: Generate) -> TaskState:
-    assert state.choices, "If using MCQ then state must have `choices` field"
-    letters, choices = letters_and_answer_options(state.choices)
-    state.user_prompt.text = template.format(question=state.user_prompt.text, choices=choices, letters=letters)
-    return state
-
-  return solve 
-
-
-my_solver = chain(
-  multiple_choice_format(template=TEMPLATE_MCQ),
-  generate(),
-)
-log = test_my_solver(my_solver, my_dataset)
-print(log)
+    # multi-GPU doesn't seem to work
+    # log = eval(wmdp_bio(), model=model, limit=10, model_args=dict(device="0,1"))
