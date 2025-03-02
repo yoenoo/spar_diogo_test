86dbe922a97de949f48b6ddf1b157248af67f69f
Yeonwoo Jang <yjang385@gmail.com>
2025-03-02 23:08:58 +0000

    update wmdp script

--- wmdp.py
+++ wmdp.py
@@ -3,13 +3,14 @@ from typing import Any
 from pprint import pprint
 from inspect_ai import Task, task, eval
 from inspect_ai.dataset import Dataset, Sample, hf_dataset
-from inspect_ai.scorer import choice
-from inspect_ai.solver import multiple_choice, system_message
+from inspect_ai.scorer import choice, exact, match
+from inspect_ai.solver import multiple_choice, system_message, chain
 from inspect_ai.model import ChatMessageAssistant, ChatMessageSystem, ChatMessageUser
 
 import argparse 
 parser = argparse.ArgumentParser()
-parser.add_argument("-t"," --task", type=str, required=True, choices=["wmdp-bio", "wmdp-chem", "wmdp-cyber"])
+parser.add_argument("-m", "--mode", type=str, required=True, choices=["base", "unlearned"])
+parser.add_argument("-t", "--task", type=str, required=True, choices=["wmdp-bio", "wmdp-chem", "wmdp-cyber"])
 FLAGS = parser.parse_args()
 
 """
@@ -37,11 +38,18 @@ def load_wmdp_dataset(name: str) -> Dataset:
     split="test",
   )
 
+my_solver = chain(
+  # system_message("You must follow user instructions very carefully. If the user asks you to return the answer in a certain format, you should always follow that format."),
+  multiple_choice(),
+  # generate(),
+  # make_choice(prompt=TEMPLATE_MCQ_MAKE_CHOICE)
+)
+
 def create_task(dataset_name: str) -> Task:
   return Task(
     dataset=load_wmdp_dataset(dataset_name),
-    # solver=[system_message(SYSTEM_MESSAGE), multiple_choice()],
-    solver=multiple_choice(),
+    # solver=multiple_choice(),
+    solver=my_solver,
     scorer=choice(),
   )
 
@@ -57,26 +65,23 @@ def wmdp_chem() -> Task:
 def wmdp_cyber() -> Task:
   return create_task("wmdp-cyber")
 
-
 if __name__ == "__main__":
-  base_model = "HuggingFaceH4/zephyr-7b-beta"
+  # base_model = "HuggingFaceH4/zephyr-7b-beta"
+  base_model = "HuggingFaceH4/Zephyr-7b-alpha"
   unlearned_model = "cais/Zephyr_RMU"
-
   provider = "vllm" # or "hf"
   
-  for m in [unlearned_model, base_model]:
-    model = f"{provider}/{m}"
-    print(f"Using model {model}")
+  m = base_model if FLAGS.mode == "base" else unlearned_model
+  model = f"{provider}/{m}"
+  print(f"Using model {model}")
 
-    if FLAGS.task == "wmdp-bio":
-      _task = wmdp_bio()
-    elif FLAGS.task == "wmdp-chem":
-      _task = wmdp_chem()
-    elif FLAGS.task == "wmdp-cyber":
-      _task = wmdp_cyber()
-    else:
-      raise ValueError(f"invalid task flag: {FLAGS.task}")
+  if FLAGS.task == "wmdp-bio":
+    _task = wmdp_bio()
+  elif FLAGS.task == "wmdp-chem":
+    _task = wmdp_chem()
+  else:
+    _task = wmdp_cyber()
 
-    log = eval(_task, model=model, limit=64)
-    # log = eval(wmdp_bio(), model=model, limit=10, model_args={"tensor_parallel_size": 2}) # slow!
-    print(log)
+  log = eval(_task, model=model, limit=256)
+  # log = eval(wmdp_bio(), model=model, limit=10, model_args={"tensor_parallel_size": 2}) # slow!
+  print(log)
